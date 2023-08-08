13b223c52056a7c163fa543911e369344651042c
azriek <github@azriek.fr>
2023-08-08 11:45:28 +0200

    Update index.php

--- index.php
+++ index.php
@@ -24,8 +24,8 @@ $token = $RdsAuthGenerator->createToken($clusterEndpoint . ":" . $clusterPort, $
 
 if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
     $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+//} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+//    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
 } else {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
